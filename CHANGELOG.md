564d278df9659aafaac70d42a2bcfa6124208ee2
Manatee <github@osuosl.org>
2017-11-02 16:40:50 +0000

    Automatic major-level version bump to v1.0.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ dobc CHANGELOG
 This file is used to list changes made in each version of the
 dobc cookbook.
 
+1.0.0 (2017-11-02)
+------------------
+- DOBC docker setup
+
 0.1.0
 -----
 - Initial release of dobc
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url       'https://github.com/osuosl-cookbooks/dobc/issues'
 source_url       'https://github.com/osuosl-cookbooks/dobc'
 description      'Installs/Configures dobc'
 long_description 'Installs/Configures dobc'
-version          '0.1.0'
+version          '1.0.0'
 
 depends          'firewall'
 depends          'osl-docker'
