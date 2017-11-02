5de116daa3814662cb87509973af96d6e12caabc
Lance Albertson <lance@osuosl.org>
2017-11-02 09:37:59 -0700

    Set to 20mb for writes

--- files/default/start-all.sh
+++ files/default/start-all.sh
@@ -2,7 +2,7 @@
 while IFS=, read -r port password ; do
   docker run -p "${port}":22 -h dobc --rm -e DOBC_PASSWORD="${password}" \
     --name=dobc-"${port}" -d \
-    --device-write-bps /dev/vda:10mb \
+    --device-write-bps /dev/vda:20mb \
     -c 10 -m 128m \
     osuosl/dobc-centos
 done < "$1"
--- files/default/start-container.sh
+++ files/default/start-container.sh
@@ -3,7 +3,7 @@ while IFS=, read -r port password ; do
   if [ "$2" == "$port" ] ; then
     docker run -p "${port}":22 -h dobc --rm -e DOBC_PASSWORD="${password}" \
       --name=dobc-"${port}" -d \
-      --device-write-bps /dev/vda:10mb \
+      --device-write-bps /dev/vda:20mb \
       -c 10 -m 128m \
       osuosl/dobc-centos
   fi
