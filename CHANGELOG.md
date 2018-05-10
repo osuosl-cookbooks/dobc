efc46408e6a77023ef88b53d77efd9caae4a4ee2
Manatee <github@osuosl.org>
2018-05-10 21:34:20 +0000

    Automatic patch-level version bump to v1.0.2 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ dobc CHANGELOG
 This file is used to list changes made in each version of the
 dobc cookbook.
 
+1.0.2 (2018-05-10)
+------------------
+- Setup MySQL containers for each shell container
+
 1.0.1 (2018-05-09)
 ------------------
 - Add support for port 8080
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url       'https://github.com/osuosl-cookbooks/dobc/issues'
 source_url       'https://github.com/osuosl-cookbooks/dobc'
 description      'Installs/Configures dobc'
 long_description 'Installs/Configures dobc'
-version          '1.0.1'
+version          '1.0.2'
 
 depends          'firewall'
 depends          'osl-docker'
