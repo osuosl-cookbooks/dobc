8b11e8d7720e66bf2f4fec18c7724d8c8ea16d4b
Catherine Stevens <catherine@crimson.workstation.cass.oregonstate.edu>
2019-04-09 13:11:40 -0700

    removed centos 6 and log_level

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,13 +6,6 @@ ChefSpec::Coverage.start! { add_filter 'dobc' }
 CENTOS_7 = {
   platform: 'centos',
   version: '7.4.1708',
-  log_level: :fatal,
-}.freeze
-
-CENTOS_6 = {
-  platform: 'centos',
-  version: '6.9',
-  log_level: :fatal,
 }.freeze
 
 RSpec.configure do |config|
