d7e3b7fcf743dda3294f65a76aa4a22ea604eea0
Manatee <github@osuosl.org>
2021-04-08 00:03:53 +0000

    Automatic minor-level version bump to v2.2.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ dobc CHANGELOG
 This file is used to list changes made in each version of the
 dobc cookbook.
 
+2.2.0 (2021-04-08)
+------------------
+- Update Chef dependency to >= 16
+
 2.1.0 (2020-09-25)
 ------------------
 - Chef 16 fixes
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ chef_version     '>= 16.0'
 issues_url       'https://github.com/osuosl-cookbooks/dobc/issues'
 source_url       'https://github.com/osuosl-cookbooks/dobc'
 description      'Installs/Configures dobc'
-version          '2.1.0'
+version          '2.2.0'
 
 depends          'base'
 depends          'firewall'
